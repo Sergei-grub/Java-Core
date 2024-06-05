5bc7aff1b89bd49905388151b3f5b98ffb83ecdf
Sergei <sergeygrubber@gmail.com>
2024-06-05 22:14:13 +0300

    Lesson 1

---
+++ Lesson_1/src/ru/gb/Simple.java
@@ -0,0 +1,18 @@
+package ru.gb;
+
+import ru.bg.Complex;
+
+/**
+ * Это простой класс. Он простой настолько, что ничего не делает
+ */
+public class Simple {
+    /**
+     * Функция запускающая программу и приветствующая мир.
+     * Наверное, самая популярная функция в мире.
+     *
+     * @param args стандартные аргументы командной строки
+     */
+    public static void main(String[] args) {
+        System.out.println(Complex.hello());
+    }
+}
